5d0530aa1240af74df4abefce5eed8fde7aa0e92
laraynnysantana <dalyllalaraynny@gmail.com>
2026-04-08 16:07:58 -0300

    feat(ex04):secreto.

---
+++ src/ex04atividades.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ex04atividades {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        ;
+        int num = 1;
+        while (num != 7) {
+            IO.println("digite o numero secreto");
+            num = sc.nextInt();
+
+        }
+    }
+}
+
